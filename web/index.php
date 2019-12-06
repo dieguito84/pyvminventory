be5a9d927c3f89d748261f45072c22596cd88350
dieguito84 <diego.mariani84@gmail.com>
2019-12-06 17:12:08 +0100

    added console link to index.php

--- web/index.php
+++ web/index.php
@@ -29,7 +29,7 @@ $hostlist = "hostlist.txt";
 $array = file($hostlist, FILE_IGNORE_NEW_LINES);
 foreach ($array as $file){
   $xml = simplexml_load_file("xml/" . $file . ".xml");
-  echo "<strong>" . $file . "</strong>" . " - last update: " . date ("F d Y H:i:s", filemtime("xml/" . $file . ".xml"));
+  echo "<strong>" . $file . "</strong>" . " - last update: " . date ("F d Y H:i:s", filemtime("xml/" . $file . ".xml")) . " - " . "<a href=" . $xml->hypervisor . " target=" . "_blank" . ">Console</a>";
   echo "<table style='width:100%'>";
   echo "<tr>";
   echo "<th width='2%'>Vmid</th>";
